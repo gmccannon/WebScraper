b6ffd27edd065759bfa21a7505724483ce99b146
gmccannon <gwm13@uakron.edu>
2024-05-17 12:33:21 -0400

    Flip requests

--- Program.cs
+++ Program.cs
@@ -14,25 +14,25 @@ namespace WebScraper
 
             using HttpClient client = new HttpClient();
             HtmlDocument htmlDoc = new HtmlDocument();
+
+            try
+            {
+                var response = await client.GetStringAsync(url);
+                htmlDoc.LoadHtml(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"The was an error with the request: {ex.Message}");
+            }
             
             StringWriter sw = new StringWriter();
             FileStream fs = new FileStream("htmlOut.xml", FileMode.Create);
             XmlTextWriter xw = new XmlTextWriter(sw);
 
-            try
-            {
-                var response = await client.GetStringAsync(url);
+            htmlDoc.OptionOutputAsXml = true; 
+            htmlDoc.Save(xw);
+            htmlDoc.Save(fs); // also saving here for testing
 
-                htmlDoc.LoadHtml(response);
-
-                htmlDoc.OptionOutputAsXml = true; 
-                htmlDoc.Save(xw);
-                htmlDoc.Save(fs); // also saving here for testing
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
         }
     }
 }
