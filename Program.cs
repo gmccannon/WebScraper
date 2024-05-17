8bfa0667555b500bf832caf7a65a7c9a020f6078
gmccannon <gwm13@uakron.edu>
2024-05-17 11:00:58 -0400

    Add basic web parser

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Xml;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            using HttpClient client = new HttpClient();
+            HtmlDocument htmlDoc = new HtmlDocument();
+            
+            StringWriter sw = new StringWriter();
+            FileStream fs = new FileStream("FileStream.xml", FileMode.Create);
+            XmlTextWriter xw = new XmlTextWriter(sw);
+
+            try
+            {
+                Console.WriteLine("Enter a URL to parse: ");
+                var url = Console.ReadLine();
+
+                var response = await client.GetStringAsync(url);
+
+                htmlDoc.LoadHtml(response);
+
+                htmlDoc.OptionOutputAsXml = true; 
+                htmlDoc.Save(xw);
+                htmlDoc.Save(fs); // also saving here for testing
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
